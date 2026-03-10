70190de514296edfc57ea04d614d20204cbc4928
abhishekkarad3-sketch <abhishekkarad3@gmail.com>
2026-03-11 02:03:44 +0530

    Update app.py

--- city_portal/city_portal/app.py
+++ city_portal/city_portal/app.py
@@ -322,4 +322,6 @@ def delete_tech(tid):
     return redirect(url_for('admin_panel'))
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
